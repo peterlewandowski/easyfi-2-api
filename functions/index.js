e14e39d255c15bc8873c2376c97b7e67ea144ddb
Peter Lewandowski <mapiotr2010@gmail.com>
2023-04-20 23:03:43 -0400

    initial function setup

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,16 @@
 const functions = require("firebase-functions");
+import functions from 'firebase-functions'
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+app.use(cors())
+
+app.get('/test', (req, res) => {
+    res.status(200).send({message: 'Success!'})
+})
+
+export const api = functions.https.onRequest(app)
 
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
@@ -7,3 +19,4 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
